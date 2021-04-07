0ddbda0bbd1d59d6f353058e58527aa711045979
sethpow <sethmadspowelson@gmail.com>
2021-04-07 13:42:17 -0600

    loads blockchain data

--- src/components/App.js
+++ src/components/App.js
@@ -16,6 +16,7 @@ class App extends Component {
     await this.loadBlockchainData()
   }
 
+  // Connecting app to blockchain; takes ethereum provider from metamask and injects into app
   async loadWeb3() {
     if (window.ethereum) {
       window.web3 = new Web3(window.ethereum)
@@ -29,27 +30,52 @@ class App extends Component {
     }
   }
 
+  // Takes info from blockchain and puts it into the app
   async loadBlockchainData() {
     const web3 = window.web3
     //Load accounts
-    //Add first account the the state
-
-    //Get network ID
-    //Get network data
-    //Check if net data exists, then
-      //Assign dvideo contract to a variable
-      //Add dvideo to the state
-
-      //Check videoAmounts
-      //Add videAmounts to the state
-
-      //Iterate throught videos and add them to the state (by newest)
+    const accounts = await web3.eth.getAccounts()
+    console.log(accounts);
+    //Add first account to the state
+    this.setState({account: accounts[0]})
 
 
-      //Set latest video and it's title to view as default 
-      //Set loading state to false
+    //Get network ID; gives current network we are connected to
+    const networkId = await web3.eth.net.getId()
+    //Get network specific data
+    const networkData = DVideo.networks[networkId]
+    // Only do next line if network id exists; Check if net data exists, then...
+    if(networkData){
+      // Create "new" JS version of contract so fn's can be called; web3 contract using abi file
+      // pass in ABI, and address/location of SC on blockchain; address of networkId will give us a copy of the smart contract
+      const dvideo = new web3.eth.Contract(DVideo.abi, networkData.address)
+      // console.log(dvideo) // logs the smart contract
+      this.setState({ dvideo })
 
-      //If network data doesn't exisits, log error
+      const videosCount = await dvideo.methods.videoCount().call()
+      this.setState({ videosCount })
+
+      // Load videos, sorts by newest
+      for (let i = videosCount; i >= 1; i--) {
+        // uses videos mapping
+        const video = await dvideo.methods.videos(i).call()
+        this.setState({
+          videos: [ ...this.state.videos, video ]
+        })
+      }
+
+      // set latest video with title to view as default
+      const latest = await dvideo.methods.videos(videosCount).call()
+      this.setState({
+        currentHash: latest.hash,
+        currentTitle: latest.title
+      })
+      this.setState({ loading: false })
+
+    } else {
+      //If network data doesn't exists, log error
+      window.alert('DVideo contract is not deployed to the detected network.')
+    }
   }
 
   //Get video
@@ -70,8 +96,14 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      loading: false
       //set states
+      buffer: null,
+      account: "",
+      dvideo: null,
+      videos: [],
+      loading: true,
+      currentHash: null,
+      currentTitle: null
     }
 
     //Bind functions
@@ -82,6 +114,7 @@ class App extends Component {
       <div>
         <Navbar 
           //Account
+          account={this.state.account}
         />
         { this.state.loading
           ? <div id="loader" className="text-center mt-5"><p>Loading...</p></div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -19,10 +19,22 @@ class Navbar extends Component {
         <ul className="navbar-nav px-3">
           <li className="nav-item text-nowrap d-none d-sm-none d-sm-block">
             <small className="text-secondary">
-              <small id="account">{/* Account... */}</small>
+              <b className="text-white">
+              <small id="account">{this.props.account}</small>
+              </b>
             </small>
               {/* Return Account&Identicon... */}
-              <b className="text-white">0x0</b>
+              {
+                this.props.account ?
+                <img
+                  className='ml-2'
+                  width='30'
+                  height='30'
+                  src={`data:image/png;base64,${new Identicon(this.props.account, 30).toString()}`}
+                  alt=""
+                /> :
+                <span></span>
+              }
           </li>
         </ul>
       </nav>
